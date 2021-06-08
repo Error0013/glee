5bc757588597ce9d43514ed72d40fc2627420513
Error0013 <1raul3@gmail.com>
2021-06-08 20:36:14 +0300

    дз15

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,49 @@
 $(function () {
+
+  $('.shop-content__btn').on('click', function () {
+    $('.shop-content__btn').removeClass('shop-content__btn--active');
+    $(this).addClass('shop-content__btn--active');
+  });
+
+  $('.button-list').on('click', function () {
+    $('.shop-content__items').addClass('shop-content__items--list')
+  })
+
+  $('.button-grid').on('click', function () {
+    $('.shop-content__items').removeClass('shop-content__items--list')
+  })
+
+  $(".star").rateYo({
+    starWidth: "12px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "5px",
+    readOnly: true,
+    "starSvg": '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="13pt" height="12pt" viewBox="0 0 13 12" version="1.1"><g id="surface1"><path style=" stroke:none;fill-rule:nonzero;" d="M 5.851562 0.417969 L 4.378906 3.519531 L 1.082031 4.019531 C 0.488281 4.109375 0.253906 4.867188 0.679688 5.300781 L 3.066406 7.714844 L 2.503906 11.125 C 2.402344 11.738281 3.027344 12.203125 3.550781 11.914062 L 6.5 10.304688 L 9.449219 11.914062 C 9.972656 12.199219 10.597656 11.738281 10.496094 11.125 L 9.933594 7.714844 L 12.320312 5.300781 C 12.746094 4.867188 12.511719 4.109375 11.917969 4.019531 L 8.621094 3.519531 L 7.148438 0.417969 C 6.882812 -0.136719 6.117188 -0.144531 5.851562 0.417969 Z M 5.851562 0.417969 "/></g></svg>',
+  });
+
+  $(".star-list").rateYo({
+    starWidth: "20px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "10px",
+    readOnly: true,
+    "starSvg": '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="13pt" height="12pt" viewBox="0 0 13 12" version="1.1"><g id="surface1"><path style=" stroke:none;fill-rule:nonzero;" d="M 5.851562 0.417969 L 4.378906 3.519531 L 1.082031 4.019531 C 0.488281 4.109375 0.253906 4.867188 0.679688 5.300781 L 3.066406 7.714844 L 2.503906 11.125 C 2.402344 11.738281 3.027344 12.203125 3.550781 11.914062 L 6.5 10.304688 L 9.449219 11.914062 C 9.972656 12.199219 10.597656 11.738281 10.496094 11.125 L 9.933594 7.714844 L 12.320312 5.300781 C 12.746094 4.867188 12.511719 4.109375 11.917969 4.019531 L 8.621094 3.519531 L 7.148438 0.417969 C 6.882812 -0.136719 6.117188 -0.144531 5.851562 0.417969 Z M 5.851562 0.417969 "/></g></svg>',
+  });
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
   $('.trendy-slider__items').slick({
     dots: true,
     arrows: false,
@@ -25,7 +70,7 @@ $(function () {
       {
         breakpoint: 576,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
         }
       }
@@ -44,13 +89,18 @@ $(function () {
     $('.menu__button').toggleClass('menu__button--active');
   });
 
+  $('.menu__button').on('click', function () {
+    $('body').toggleClass('lock-scroll');
+  });
+
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
+    if ($(this).scrollTop() > 1) {
       $('.header__top').addClass('header__top--fixed');
     } else {
       $('.header__top').removeClass('header__top--fixed');
     }
   });
+
 
   var containerEl1 = document.querySelector('[data-ref="container-1"]');
   var containerEl2 = document.querySelector('[data-ref="container-2"]');
